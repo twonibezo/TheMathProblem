d84878aeef6d7d3fca5835aa817d908b3f4e38aa
twonibezo <nibezo981pro@gmail.com>
2018-06-12 14:00:39 +0300

    The end program.

--- Main.py
+++ Main.py
@@ -1,16 +1,14 @@
-arrayX = list(range(-30,31))
-arrayY = list(range(-30,31))
-arrayZ = list(range(-30,31))
+x = list(range(-30,31))
+y = list(range(-30,31))
+z = list(range(-30,31))
+print ('The solves of the equations is here: ')
+for i in x:
+    for u in y:
+        for c in z:
+            if (x[i]**3+y[u]**3+z[c]**3) == 8:
+                w = x[i], y[u], z[c]
+                if x[i] != y[u] and y[u] != z[c] and x[i] != z[c]:
+                    if x[i] + y[u] + z[c] == 8:
+                        print(w)
 
-x = y = z = 0
-a = 0
-count = 0
 
-while a < 61:
-    count = arrayX[x+1] + arrayY[y+1] + arrayZ[z+1]
-    a = a+1
-    x = x + 1
-    y = y + 1
-    z = z + 1
-    if count >= 0:
-        print (count)
